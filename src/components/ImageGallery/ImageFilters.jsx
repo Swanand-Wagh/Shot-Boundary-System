9c9738ade0b0801582481b661d812bdef3a4aabf
Swanand-Wagh <swanandwagh7@gmail.com>
2023-11-28 23:30:18 -0800

    misc

--- src/components/ImageGallery/ImageFilters.jsx
+++ src/components/ImageGallery/ImageFilters.jsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { RiImageAddFill } from 'react-icons/ri';
 import { videoArray } from '../../constants/VideosList';
-import ReactPlayer from 'react-player';
-// import video from '../../constants/videos/0.mp4';
 
 export const ImageFilters = ({ currentImg, imagesList }) => {
   const [currentImgURL, setCurrentImageURL] = useState('');
@@ -23,20 +21,14 @@ export const ImageFilters = ({ currentImg, imagesList }) => {
   }, [currentImg, imagesList]);
 
   return (
-    // <ReactPlayer url="../../constants/videos/0.mp4" />
-
     <div className="imageGallery__contentWraps imageSelectDisplay">
-      <p>{currentVideo}</p>
       <div className="imageGallery__imageSelectDisplay__selectedImage">
         <div
           style={{ background: currentImg !== -1 && currentImgURL ? 'transparent' : '#efedfc' }}
           className="imageGallery__imageSelectDisplay__selectedImage__imageWrap"
         >
           {currentImg !== -1 && currentImgURL ? (
-            <video autoPlay controls>
-              <source src="/videos/0.mp4" type="video/mp4" />
-              Sorry, your browser doesn't support videos.
-            </video>
+            <video controls muted src={`/videos/${currentImg}.mp4`}></video>
           ) : (
             <div className="imageGallery__imageSelectDisplay__selectedImage__noImage">
               <span className="imageGallery__imageSelectDisplay__selectedImage__noImage__noImgIcon">
